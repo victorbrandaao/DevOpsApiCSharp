bc56bd62acdd9658bd00911e9d27912a4b814618
victorbrandaao <victorbrandaotech@gmail.com>
2025-08-14 11:30:24 -0300

    Adiciona endpoints para gerenciamento de tarefas na API

--- Program.cs
+++ Program.cs
@@ -4,12 +4,17 @@ var app = builder.Build();
 // Lista de tarefas (em memória)
 var tasks = new List<string>();
 
+
+
 app.MapGet("/", () => "API DevOps em C#!");
 app.MapGet("/tasks", () => tasks);
 app.MapPost("/tasks", (string task) =>
 {
     tasks.Add(task);
     return Results.Ok(new { message = "Tarefa adicionada com sucesso!" });
+
 });
 
 app.Run();
+
+public partial class Program { }
